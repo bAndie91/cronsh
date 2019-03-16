478c15ae218e1597c7abc0f23711a057e42d3c1d
Andreas Hrubak <bandie9100@gmail.com>
2019-03-17 00:54:11 +0100

    add cronsh_loadenv native helper program

---
+++ cronsh_loadenv.c
@@ -0,0 +1,57 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <err.h>
+#include <string.h>
+
+#define SETENV_OVERWRITE 1
+
+int main(int argc, char** argv)
+{
+	FILE * envfile;
+	char buf[4096];
+	char * s;
+	char * val;
+	
+	if(argc > 2)
+	{
+		if(envfile = fopen(argv[1], "r"))
+		{
+			while(fgets(buf, sizeof(buf), envfile))
+			{
+				if((buf[0] != '#') && (s = strchr(buf, '=')))
+				{
+					*s = '\0';
+					val = s + 1;
+					if(s = strchr(val, '\n')) *s = '\0';
+					
+					if(strlen(val) > 0) {
+						if(setenv(buf, val, SETENV_OVERWRITE)!=0) goto fail;
+					}
+					else {
+						if(unsetenv(buf)!=0) goto fail;
+					}
+				}
+			}
+		}
+		else
+		{
+			if(errno != ENOENT)  /* ignore if file does not exist */
+			{
+				perror("fopen");
+				goto fail;
+			}
+		}
+		
+		execvp(argv[2], &argv[2]);
+		goto fail;
+	}
+	else
+	{
+		warnx("Usage: loadenv [env-file] [command] [arguments [...]]");
+	}
+	
+	fail:
+	return errno == 0 ? -1 : errno;
+}
